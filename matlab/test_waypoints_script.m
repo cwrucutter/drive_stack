f7dd3a6e4819493f0caaeb9042c12a1c40e178d5
Matthew Klein <ggallin08@gmail.com>
2017-12-28 15:31:15 -0500

    Plot more things

--- matlab/test_waypoints_script.m
+++ matlab/test_waypoints_script.m
@@ -1,15 +1,28 @@
 xPre = rand(1,2);
-thetaPre = 2*pi*rand;
+thetaPre = 2*pi*rand-pi;
 vPre = 2*rand;
 
 xTarg = rand(1,2);
-thetaTarg = 2*pi*rand;
+thetaTarg = 2*pi*rand-pi;
 vTarg = 2*rand;
+
+xPre = [0 0];
+thetaPre = 0;
+vPre = 2;
+xTarg = [1 2];
+thetaTarg = pi/2;
+vTarg = 2;
 
 dt = 0.05;
 t = 0:dt:1;
 
 [X,V,A,P] = connect_waypoints(xPre,thetaPre,vPre,xTarg,thetaTarg,vTarg,dt);
+
+vTot = sqrt(V(:,1).^2 + V(:,2).^2);
+theta = atan2(V(:,2),V(:,1));
+
+assert(vTot(1) - vPre < 0.00000001)
+assert(vTot(end) - vTarg < 0.00000001)
 
 figure(1)
 plot(X(:,1),X(:,2),'.r')
@@ -23,7 +36,34 @@ hold off
 figure(2)
 subplot(3,1,1)
 plot(t,X(:,1),t,X(:,2))
+ylabel("x,y")
+xlabel("t")
 subplot(3,1,2)
 plot(t,V(:,1),t,V(:,2))
+ylabel("v_x,v_y")
+xlabel("t")
 subplot(3,1,3)
 plot(t,A(:,1),t,A(:,2))
+ylabel("a_x,a_y")
+xlabel("t")
+
+figure(3)
+subplot(3,1,1)
+plot(t,X(:,1),t,X(:,2))
+ylabel("x,y")
+xlabel("t")
+subplot(3,1,2)
+plot(t,vTot)
+hold on
+plot(0,vPre,'+r',1,vTarg,'+r')
+hold off
+ylabel("v_{tot}")
+xlabel("t")
+subplot(3,1,3)
+plot(t,theta)
+hold on
+plot(0,thetaPre,'+r',1,thetaTarg,'+r')
+hold off
+ylabel("\theta")
+xlabel("t")
+ylim([-pi pi])
