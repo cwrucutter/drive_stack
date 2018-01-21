2af2b499d6c6c5993d1edadc493888d9a906beaf
Matthew Klein <ggallin08@gmail.com>
2018-01-21 01:06:18 -0500

    Calculate omega

--- matlab/test_waypoints_script.m
+++ matlab/test_waypoints_script.m
@@ -20,6 +20,14 @@ t = 0:dt:1;
 
 vTot = sqrt(V(:,1).^2 + V(:,2).^2);
 theta = atan2(V(:,2),V(:,1));
+kappa = (V(:,1).*A(:,2) - V(:,2).*A(:,1))./((V(:,1).^2 + V(:,2).^2).^(3/2));
+omega = vTot.*kappa;
+
+% 
+%            x'y" - y'x"
+% κ(t)  = --------------------
+%          (x'² + y'²)^(3/2)
+%
 
 assert(vTot(1) - vPre < 0.00000001)
 assert(vTot(end) - vTarg < 0.00000001)
@@ -48,18 +56,18 @@ ylabel("a_x,a_y")
 xlabel("t")
 
 figure(3)
-subplot(3,1,1)
+subplot(4,1,1)
 plot(t,X(:,1),t,X(:,2))
 ylabel("x,y")
 xlabel("t")
-subplot(3,1,2)
+subplot(4,1,2)
 plot(t,vTot)
 hold on
 plot(0,vPre,'+r',1,vTarg,'+r')
 hold off
 ylabel("v_{tot}")
 xlabel("t")
-subplot(3,1,3)
+subplot(4,1,3)
 plot(t,theta)
 hold on
 plot(0,thetaPre,'+r',1,thetaTarg,'+r')
@@ -67,3 +75,12 @@ hold off
 ylabel("\theta")
 xlabel("t")
 ylim([-pi pi])
+subplot(4,1,4)
+plot(t,omega)
+hold on
+omegaPre = 0;
+omegaTarg = 0;
+plot(0,omegaPre,'+r',1,omegaTarg,'+r')
+hold off
+ylabel("\omega")
+xlabel("t")
